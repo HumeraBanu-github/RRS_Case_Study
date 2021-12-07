8d1a3de547c7cb8e01c7d663482b5f565db21b24
HumeraBanu-github <93283737+HumeraBanu-github@users.noreply.github.com>
2021-12-07 10:25:15 +0530

    Add files via upload

---
+++ MicroServices with HelloWorld/Contact Service/ContactResource.java
@@ -0,0 +1,11 @@
+package com.RrsCaseStudy.Resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.RrsCaseStudy.model.Contact;
+
+@RestController
+public class ContactResource {
+	
+}
